c9c755ddd5c9ac4a38a627771072d63e6e747b83
Danil Zharkov <zharkov.danik.ua@gmail.com>
2026-01-13 22:37:39 -0700

    deploy

--- frontend/app/products/service.ts
+++ frontend/app/products/service.ts
@@ -2,12 +2,12 @@ import axios from 'axios'
 
 export const service = {
   getAllProducts: async () => {
-    const res = await axios.get('http://localhost:3000/products')
+    const res = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/products`)
     return res.data
   },
   getProductCheckout: async (productId: number) => {
     const res = await axios.get(
-      `http://localhost:3000/products/${productId}/checkout`
+      `${process.env.NEXT_PUBLIC_API_URL}/products/${productId}/checkout`
     )
     return res.data
   },

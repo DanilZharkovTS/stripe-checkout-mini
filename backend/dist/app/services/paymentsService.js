271a89a369861339b7c048c068412b8c79f0dbdd
Danil Zharkov <zharkov.danik.ua@gmail.com>
2026-01-14 21:51:06 -0700

    fix: build version of app

--- backend/dist/app/services/paymentsService.js
+++ backend/dist/app/services/paymentsService.js
@@ -30,8 +30,8 @@ export const paymentsService = {
                     quantity: 1,
                 },
             ],
-            success_url: 'http://localhost:3001/checkout/success',
-            cancel_url: 'http://localhost:3001/products',
+            success_url: `${process.env.FRONTEND_URL}/checkout/success`,
+            cancel_url: `${process.env.FRONTEND_URL}/products`,
         });
         paymentsRepo.updateOrderSessionId(session.id, dbOrder.id);
         return { checkoutUrl: session.url };
